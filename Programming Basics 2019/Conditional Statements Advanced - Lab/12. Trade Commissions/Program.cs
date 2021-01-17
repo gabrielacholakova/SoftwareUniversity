ecf369aa687d159c10d86e25ac51dc7f0194f1bd
gabrielacholakova <gabriela.cholakova3@gmail.com>
2021-01-17 19:52:40 +0000

    Conditional Statements Advanced - Lab

---
+++ Programming Basics 2019/Conditional Statements Advanced - Lab/12. Trade Commissions/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace _12._Trade_Commissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = Console.ReadLine();
+            double sales = double.Parse(Console.ReadLine());
+            double commision = 0;
+
+            if (city == "Sofia")
+            {
+                if (sales >=0 && sales <=500)
+                {
+                    commision = (sales / 100) * 5;
+                }
+                else if(sales > 500 && sales <= 1000)
+                {
+                    commision = (sales / 100) * 7;
+                }
+                else if (sales > 1000 && sales <= 10000)
+                {
+                    commision = (sales / 100) * 8;
+                }
+                else if (sales > 10000)
+                {
+                    commision = (sales / 100) * 12;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                    return;
+                }
+            }
+            else if (city == "Varna")
+            {
+                if (sales >= 0 && sales <= 500)
+                {
+                    commision = (sales / 100) * 4.5;
+                }
+                else if (sales > 500 && sales <= 1000)
+                {
+                    commision = (sales / 100) * 7.5;
+                }
+                else if (sales > 1000 && sales <= 10000)
+                {
+                    commision = (sales / 100) * 10;
+                }
+                else if (sales > 10000)
+                {
+                    commision = (sales / 100) * 13;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                    return;
+                }
+            }
+            else if (city =="Plovdiv")
+            {
+                if (sales >= 0 && sales <= 500)
+                {
+                    commision = (sales / 100) * 5.5;
+                }
+                else if (sales > 500 && sales <= 1000)
+                {
+                    commision = (sales / 100) * 8;
+                }
+                else if (sales > 1000 && sales <= 10000)
+                {
+                    commision = (sales / 100) * 12;
+                }
+                else if (sales > 10000)
+                {
+                    commision = (sales / 100) * 14.5;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                    return;
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("error");
+                return;
+            }
+
+            Console.WriteLine($"{commision:F2}");
+        }
+    }
+}
---
+++ Programming Basics 2019/Conditional Statements Advanced - Lab/13. Ski Trip/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace _13._Ski_Trip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double days = double.Parse(Console.ReadLine());
+            string type = Console.ReadLine();
+            string opinion = Console.ReadLine();
+            double price = 0;
+            double moneyNeeded = 0;
+            double discount = 0;
+            double moneyAfterdiscount = 0;
+            double moreDiscount = 0;
+            double totalSum = 0;
+
+            if ( type == "room for one person")
+            {
+                price = 18.00;
+                moneyNeeded = (days * price) - price;
+                if (opinion == "positive")
+                {
+                    discount = (moneyNeeded / 100) * 25;
+                    totalSum = discount + moneyNeeded;
+                }
+               else if (opinion == "negative")
+                {
+                    discount = (moneyNeeded / 100) * 10;
+                    totalSum = moneyNeeded - discount;
+                }
+                
+                Console.WriteLine($"{totalSum:F2}");
+                return;
+                
+            }
+            else if (type == "apartment")
+            {
+                price = 25.00;
+                moneyNeeded = (days * price) - price;
+                if (days < 10)
+                {
+                    discount = (moneyNeeded / 100) * 30;
+                    moneyAfterdiscount = moneyNeeded - discount;
+                }
+                else if (days > 10 && days <= 15)
+                {
+                    discount = (moneyNeeded / 100) * 35;
+                    moneyAfterdiscount = moneyNeeded - discount;
+                }
+                else if (days > 15)
+                {
+                    discount = (moneyNeeded / 100) * 50;
+                    moneyAfterdiscount = moneyNeeded - discount;
+                }
+            }
+            else if (type == "president apartment")
+            {
+                price = 35.00;
+                moneyNeeded = (days * price) - price;
+                if (days < 10)
+                {
+                    discount = (moneyNeeded / 100) * 10;
+                    moneyAfterdiscount = moneyNeeded - discount;
+                }
+                else if (days > 10 && days <= 15)
+                {
+                    discount = (moneyNeeded / 100) * 15;
+                    moneyAfterdiscount = moneyNeeded - discount;
+                }
+                else if (days > 15)
+                {
+                    discount = (moneyNeeded / 100) * 20;
+                    moneyAfterdiscount = moneyNeeded - discount;
+                }
+            }
+
+
+            if (opinion == "positive")
+            {
+                moreDiscount = (moneyAfterdiscount / 100) * 25;
+                totalSum = moneyAfterdiscount + moreDiscount;
+            }
+            else if (opinion == "negative")
+            {
+                moreDiscount = (moneyAfterdiscount / 100) * 10;
+                totalSum = moneyAfterdiscount - moreDiscount;
+            }
+
+            Console.WriteLine($"{totalSum:F2}");
+
+            
+
+
+        }
+    }
+}
