30b5539a1fbd2c921d95a06b9cfbf6c00908384d
gabrielacholakova <gabriela.cholakova3@gmail.com>
2021-01-17 19:28:51 +0000

    Conditional Statements - Lab

---
+++ Programming Basics 2019/Conditional Statements - Lab/02. Greater Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02._Greater_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num1 >= num2)
+            {
+                Console.WriteLine(num1);
+            }
+            else if (num2 >= num1)
+            {
+                Console.WriteLine(num2);
+            }
+                
+              
+           
+            }
+        }
+    }
+
---
+++ Programming Basics 2019/Conditional Statements - Lab/07. Toy Shop/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _07._Toy_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vacationPrice = double.Parse(Console.ReadLine());
+            double countOfPuzzles = double.Parse(Console.ReadLine());
+            double countOfDolls = double.Parse(Console.ReadLine());
+            double countOfBears = double.Parse(Console.ReadLine());
+            double countOfMinions = double.Parse(Console.ReadLine());
+            double countOfTrucks = double.Parse(Console.ReadLine());
+            
+            double sum = (countOfPuzzles * 2.6) + (countOfDolls * 3) + (countOfBears * 4.10) + (countOfMinions * 8.20) + (countOfTrucks * 2);
+            double countOfToys = countOfPuzzles + countOfDolls + countOfBears + countOfMinions + countOfTrucks;
+
+            if (countOfToys >= 50)
+            {
+                double discount = (sum / 100) * 25;
+                double priceAfterDiscount = sum - discount;
+                double rent = priceAfterDiscount / 10;
+                double profit = priceAfterDiscount - rent;
+                if( profit >= vacationPrice)
+                {
+                    double moneyLeft = profit - vacationPrice;
+                    Console.WriteLine($"Yes! {moneyLeft:F2} lv left.");
+                }
+                else
+                {
+                    double moneyNeeded = vacationPrice - profit;
+                    Console.WriteLine($"Not enough money! {moneyNeeded:F2} lv needed.");
+                }
+            }
+            else
+            {
+                double rent = sum / 10;
+                double profit = sum - rent;
+                if (profit > vacationPrice)
+                {
+                    double moneyLeft = profit - vacationPrice;
+                    Console.WriteLine($"Yes! {moneyLeft:F2} lv left.");
+                }
+                else
+                {
+                    double moneyNeeded = vacationPrice - profit;
+                    Console.WriteLine($"Not enough money! {moneyNeeded:F2} lv needed.");
+                }
+            }
+
+            
+            
+        }
+    }
+}
